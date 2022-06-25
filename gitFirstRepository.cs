0c305cfb8d7fb2373ebe8961303a1225576932e7
Frederik Ibara <frederikibara@gmail.com>
2022-06-25 18:18:13 +0300

    Add gitFirstRepository.cs

---
+++ gitFirstRepository.cs
@@ -0,0 +1,14 @@
+using System.Console;
+using Console.Write;
+
+namespace gitFirstRepository{
+	
+	
+	class GiTFirstRepository{
+	
+	public static void Main(string [] args){
+		
+    System.Console.Write("Hello");
+}
+}
+}
